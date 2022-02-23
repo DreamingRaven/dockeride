bc7764f30795a9a7e305aa338cd4ee0c04574b3b
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-02-23 11:35:32 +0000

    Added sample python file
    Adding sample files to encourage experimentation and exploration of
    syntax highlighting and autocompletion. So for example this file
    highlighted that syntax highlighting made no distinction between
    docstrings and normal strings.

---
+++ samples/sample.py
@@ -0,0 +1,30 @@
+# Sample python file to check syntax highlighting and language server
+import platform
+
+class Sample():
+    """Sample Python class."""
+
+    def __init__(self, arg=None):
+        """Another doc string for the class."""
+        self.mproperty = arg if arg is not None else 0
+
+    @property
+    def mproperty(self):
+        if self.__dict__.get("_mproperty") == None:
+            self._mproperty = 1
+        return self._mproperty
+
+    @mproperty.setter
+    def mproperty(self, new):
+        self._mproperty = new
+
+def main():
+    """Sample function for highlighting and completion testing."""
+    print("External package syntax highlighting: {}".format(
+        platform.platform()))
+    print("Internal class syntax highlighting and completion: {}".format(
+        Sample(arg=10).mproperty
+        ))
+
+if __name__=="__main__":
+    main()
