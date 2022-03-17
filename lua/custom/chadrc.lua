45358631e52dbf0416735827943d6c958cc52a5d
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-03-17 10:10:59 +0000

    Applied sideducks configs
    https://github.com/siduck/dotfiles/tree/master/nvchad/custom

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,12 +1,30 @@
--- Just an example, supposed to be placed in /lua/custom/
-
+-- make sure you maintain the structure of `core/default_config.lua` here,
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
+local plugin_conf = require "custom.plugins.configs"
+local userPlugins = require "custom.plugins"
+
+M.plugins = {
+   status = {
+      colorizer = true,
+   },
+
+   options = {
+      lspconfig = {
+         setup_lspconf = "custom.plugins.lspconfig",
+      },
+   },
+
+   default_plugin_config_replace = {
+      nvim_treesitter = plugin_conf.treesitter,
+      nvim_tree = plugin_conf.nvimtree,
+   },
+
+   install = userPlugins,
+}
 
 M.ui = {
-   theme = "gruvchad",
+   theme = "solarized",
 }
 
 return M
