1edf7d5f172d50f6891f87cbad20e0a470f88e5b
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-03-17 15:22:55 +0000

    Commented out unused

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -9,16 +9,16 @@ M.plugins = {
      colorizer = true,
   },
 
-  options = {
-     lspconfig = {
-        setup_lspconf = "custom.plugins.lspconfig",
-     },
-  },
-
-  default_plugin_config_replace = {
-     nvim_treesitter = plugin_conf.treesitter,
-     nvim_tree = plugin_conf.nvimtree,
-  },
+  -- options = {
+  --    lspconfig = {
+  --       setup_lspconf = "custom.plugins.lspconfig",
+  --    },
+  -- },
+  --
+  -- default_plugin_config_replace = {
+  --    nvim_treesitter = plugin_conf.treesitter,
+  --    nvim_tree = plugin_conf.nvimtree,
+  -- },
 
   install = userPlugins,
 }
