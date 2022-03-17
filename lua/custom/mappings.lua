5e3533f08f1665d6931c7c99434da8adcf230892
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-03-17 10:52:29 +0000

    Adding back some of our hotkeys

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,3 +8,7 @@ map("n", "<leader>te", ":Telescope <CR>")
 map("n", "<leader>ta", ":TZAtaraxis <CR>")
 map("n", "<leader>tm", ":TZMinimalist <CR>")
 map("n", "<leader>tf", ":TZFocus <CR>")
+
+map("n", "<C-_>", ":lua require('Comment.api').toggle_current_linewise()<CR>")
+map("v", "<C-_>", ":lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
+map("n", "<C-Bslash>", ":NvimTreeToggle<CR>")
